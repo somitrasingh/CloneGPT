51f69616a5d660508d9cfabeb9deaeb2a8b0fe6b
Somitra Singh Kushwah <somitrasingh07@gmail.com>
2024-10-25 14:49:54 -0500

    extra changes with google spreadsheet

--- sheets.js
+++ sheets.js
@@ -1,15 +1,15 @@
 const { google } = require('googleapis');
 const sheets = google.sheets('v4');
-const fs = require('fs');
+require('dotenv').config();
 
-// Load your service account key (the JSON file you downloaded)
+// Load your service account key from the environment variable
 const auth = new google.auth.GoogleAuth({
-  keyFile: './google-credentials.json', // Replace with the actual path to your key file
+  credentials: JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS), // Load credentials from the environment variable
   scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
 
 // The ID of your Google Sheet (you can find this in the URL of the sheet)
-const spreadsheetId = '1m8QB9ACtBzUHwh-XgUfhdIvboQuk7NT9zRfuhUPtEcs'; // Replace with your sheet's ID
+const spreadsheetId = process.env.SPREADSHEETID; // Replace with your sheet's ID
 
 // Function to append data to the Google Sheet
 async function addToSheet(data) {
@@ -27,7 +27,7 @@ async function addToSheet(data) {
 
   try {
     const response = await sheets.spreadsheets.values.append(request);
-    
+   
   } catch (err) {
     console.error('Error adding data to sheet:', err);
   }
